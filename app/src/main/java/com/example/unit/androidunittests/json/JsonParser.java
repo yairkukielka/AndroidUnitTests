bf0152868fccbb5f2d779ac577c09bb554d32ef9
Yair Kukielka <yair.kukielka@gmail.com>
2016-02-21 10:06:46 -0800

    Reformated, and updated comments

--- app/src/main/java/com/example/unit/androidunittests/json/JsonParser.java
+++ app/src/main/java/com/example/unit/androidunittests/json/JsonParser.java
@@ -1,18 +1,3 @@
-/*
- * Copyright 2015 Google Inc. All rights reserved.
- *
- * Licensed under the Apache License, Version 2.0 (the "License");
- * you may not use this file except in compliance with the License.
- * You may obtain a copy of the License at
- *
- *      http://www.apache.org/licenses/LICENSE-2.0
- *
- * Unless required by applicable law or agreed to in writing, software
- * distributed under the License is distributed on an "AS IS" BASIS,
- * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
- * See the License for the specific language governing permissions and
- * limitations under the License.
- */
 package com.example.unit.androidunittests.json;
 
 import com.example.unit.androidunittests.Item;
@@ -23,7 +8,6 @@ import org.json.JSONObject;
  * Json parser.
  */
 public class JsonParser {
-
     public static final String KEY_TAG = "key";
     public static final String VALUE_TAG = "value";
 
@@ -36,8 +20,8 @@ public class JsonParser {
 
     public Item deserialize(String s) throws Exception {
         JSONObject jsonObject = new JSONObject(s);
-        return new Item(jsonObject.getString(KEY_TAG), jsonObject.getBoolean(VALUE_TAG));
+        String key = jsonObject.getString(KEY_TAG);
+        boolean value = jsonObject.getBoolean(VALUE_TAG);
+        return new Item(key, value);
     }
-
-
 }
