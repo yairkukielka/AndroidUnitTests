da9ea08bd21475dc92dbb572524dff64e3067251
Yair Kukielka <yair.kukielka@gmail.com>
2016-02-21 10:04:39 -0800

    Fixed the name of the class and imported junit for instrumented tests

--- app/src/androidTest/java/com/example/unit/androidunittests/json/JsonParserTest.java
+++ app/src/androidTest/java/com/example/unit/androidunittests/json/JsonParserTest.java
@@ -12,7 +12,7 @@ import static org.junit.Assert.assertEquals;
  * Instrumented test for a json parser. It uses org.json classes from the Android SDK
  */
 @SmallTest
-public class InstrumentedJsonParserTest {
+public class JsonParserTest {
 
     @Test
     public void serialize_deserialize() throws Exception {
